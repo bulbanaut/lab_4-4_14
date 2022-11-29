c7590d99a34b5594529923da3db995efdaa49af1
bulbanaut <aleks13102004@gmail.com>
2022-11-29 21:18:32 +0500

    modified:   4.4.14.py

--- 4.4.14.py
+++ 4.4.14.py
@@ -3,4 +3,4 @@ import math
 a, b, h = int(input('a:')), int(input('b:')), int(input('h:'))
 for x in range(a, b +1, h):
     y = 2*math.tan(x/2) +1
-    print('', '_'*(len(str(y)) +6) , '\n', '|', x, '|', y)
+    print('', '_'*(len(str(y)) +8) , '\n', '|', x, '|', y, '|')
